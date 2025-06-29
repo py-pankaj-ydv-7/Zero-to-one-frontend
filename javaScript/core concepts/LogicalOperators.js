23d3c10f5a4eb2dc2f3a3f8e540112913159588b
Pankaj Yadav <pankajyadav851685@gmail.com>
2025-06-29 06:04:33 +0530

    logical operators

---
+++ javaScript/core concepts/LogicalOperators.js
@@ -0,0 +1,20 @@
+let age= 19;
+let isStudent = true;
+
+if(age >= 18 && isStudent){
+    console.log("You can eligible for internship");
+    
+}else if(age >= 18)
+{
+    console.log("You have to do a job");
+    
+}
+else{
+    console.log("YOu are neither");
+    
+}
+
+if(age <22 || isStudent){
+    console.log("YOu are eligible for scholarship");
+    
+}
