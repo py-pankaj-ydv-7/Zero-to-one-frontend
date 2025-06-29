7efc04193be1c58a460a49d8268682fdd1bc30d5
Pankaj Yadav <pankajyadav851685@gmail.com>
2025-06-30 01:00:13 +0530

    functions

--- javaScript/core concepts/function.js
+++ javaScript/core concepts/function.js
@@ -3,4 +3,21 @@ function greeting(){
     
 }
 
-greeting();
+greeting(); 
+
+
+// function getPrimeNumber(){
+//     return 7;
+
+// }
+// let prime = getPrimeNumber();
+// console.log(prime);
+
+
+function sum(first, second){
+    let add = first + second;
+    return add;
+}
+
+let result = sum(21,9);
+console.log(result);
