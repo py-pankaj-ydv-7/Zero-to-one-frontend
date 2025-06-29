66fd346435c9836e9cf8b7117a422f05da659e80
Pankaj Yadav <pankajyadav851685@gmail.com>
2025-06-30 01:31:51 +0530

    callback function

--- javaScript/core concepts/callbacks.js
+++ javaScript/core concepts/callbacks.js
@@ -3,4 +3,13 @@ function greeting(name){
     console.log("Hello, "+ name);
 }
 
-// Define
+// Define a function that takes a callback 
+
+function processUserInput(callback){
+    var name = prompt("please enter your name.");
+    callback(name);
+}
+
+//call the function with the callback 
+
+processUserInput(greeting);
