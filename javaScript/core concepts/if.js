8d3ad35fa77fdccda55e39a8932cf4bf5fc8c406
Pankaj Yadav <pankajyadav851685@gmail.com>
2025-06-29 18:52:25 +0530

    if , else , else if

--- javaScript/core concepts/if.js
+++ javaScript/core concepts/if.js
@@ -1,8 +1,36 @@
-let age = 15;
+// let age = 15;
 
-if(age >= 18){
-    console.log("You are an eligeible voter.");
-}
-else{
-    console.log("You can provide moral support")
-}
+// if(age >= 18){
+//     console.log("You are an eligeible voter.");
+// }
+// else{
+//     console.log("You can provide moral support")
+// }
+
+// Grade System *****
+
+// let marks = 40;
+// if(marks>= 90 && marks<=100){
+//     console.log("Grade A");   
+// }
+// else if(marks>=80 && marks<90){
+//     console.log("Grade B");
+    
+// }
+// else if(marks>=70 && marks<80){
+//     console.log("Grade C");
+    
+// }
+// else if (marks>=50 && marks<70){
+//     console.log("Grade D");
+    
+// }
+// else if(marks>=33 && marks<50){
+//     console.log("Grade E ");
+    
+// }
+// else(
+//     console.log("fail")
+    
+// )
+
