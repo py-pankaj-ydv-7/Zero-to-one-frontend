be26f08773afe7b854279b22ae0c11dc5d7b6a06
Pankaj Yadav <pankajyadav851685@gmail.com>
2025-06-30 01:20:08 +0530

    loops

---
+++ javaScript/core concepts/loops.js
@@ -0,0 +1,17 @@
+let counter = 0; // Initialization
+while(counter<10){ // condition
+    console.log(`${counter+1}`);
+    
+    counter = counter+1 // updation
+}
+console.log("out of the loop");
+
+
+console.log("for loop *** ");
+
+for(c = 0; c<100; c++){
+    console.log(`${c+1}`);
+    
+}
+
+console.log("out of the for loop ");
