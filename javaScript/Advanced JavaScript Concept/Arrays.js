7e6d076cd36f60507795f03ad5720cc97978861a
Pankaj Yadav <pankajyadav851685@gmail.com>
2025-07-02 03:07:29 +0530

    forEachloop and Arrays

---
+++ javaScript/Advanced JavaScript Concept/Arrays.js
@@ -0,0 +1,15 @@
+let arr = [1,2,3, 'pop', null, {channel: "KGCoding"}];
+
+console.log(arr);
+// console.log(arr[1]);
+// console.log(arr[2]);
+// console.log(arr[3]);
+
+console.log(typeof (arr));
+
+console.log("for loop with array");
+
+for(let i = 0;i<arr.length;i++){
+    console.log(arr[i]);
+    
+}
---
+++ javaScript/Advanced JavaScript Concept/forEachLoop.js
@@ -0,0 +1,13 @@
+let foods = ['bread', 'rice', 'meat', 'pizza'];
+
+foods.forEach(function(food){
+    console.log(food);
+    
+})
+
+// if you want to print index also . so you give them one or parameter variable
+
+foods.forEach(function(food, pan , val){
+    console.log(food , pan, val);
+    
+})
