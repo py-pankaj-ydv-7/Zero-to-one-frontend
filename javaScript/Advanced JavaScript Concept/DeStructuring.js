9664c344bfa0308cc56cabab865b766236a3e2d5
Pankaj Yadav <pankajyadav851685@gmail.com>
2025-07-03 01:41:11 +0530

    DeStructurring

---
+++ javaScript/Advanced JavaScript Concept/DeStructuring.js
@@ -0,0 +1,12 @@
+let product = {
+    company: 'Mango',
+    itemName: 'Cotton striped t-shirt',
+    price: 861
+};
+
+// Destructuring
+// let company = product.company;
+console.log(company); // console pe print ho gaya
+
+// is same as
+let { company } = product;
