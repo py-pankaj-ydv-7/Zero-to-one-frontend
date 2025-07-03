76d4f406cc40f3a54504376db49fcb5c75ee9055
Pankaj Yadav <pankajyadav851685@gmail.com>
2025-07-04 04:37:50 +0530

    arrow functions

---
+++ javaScript/ES6 JavaScript/arrowFunc.js
@@ -0,0 +1,16 @@
+/*************** 2. Arrow functions  */
+// purani js mein teen prakar k functions they , they were: 1. function statement, 2. function expression, 3. anonymous function
+
+// new js me ek prakar ka function hai , jiske teen roop hai:
+// arrow function
+//1. basic fat arrow function
+()=>{} // this is arrow func
+var func = ()=>{} ;// this is arrow function , store in func name variable.
+
+
+//2.fat arrow function with one parameters
+var b = (parm)=>{}; // one variable k case me hum ise hata sakte hai, parm k bracket ko 
+
+//3.fat arrow function with implicit return
+var g = ()=>12;
+console.log(g());
