a40da6f867905def1c06be6a66ff8e703de2c1a5
Pankaj Yadav <pankajyadav851685@gmail.com>
2025-07-04 04:35:00 +0530

    arrow functions

--- javaScript/ES6 JavaScript/index.js
+++ javaScript/ES6 JavaScript/index.js
@@ -1,10 +1,10 @@
-let a = 12 ; // this is variable, its value will be changed
-a = 23;
-console.log(a);
+// let a = 12 ; // this is variable, its value will be changed
+// a = 23;
+// console.log(a);
 
-const b = 15;
-// b = 455;
-console.log(b); // it gives error
+// const b = 15;
+// // b = 455;
+// console.log(b); // it gives error
 
 //let and const dono different hai , matlab let makes a variable and const makes a constant
 //let and const are similar in few things:
@@ -13,3 +13,18 @@ console.log(b); // it gives error
 //................ braces scoped hote hai
 
 /*************** 2. Arrow functions  */
+// purani js mein teen prakar k functions they , they were: 1. function statement, 2. function expression, 3. anonymous function
+
+// new js me ek prakar ka function hai , jiske teen roop hai:
+// arrow function
+//1. basic fat arrow function
+()=>{} // this is arrow func
+var func = ()=>{} ;// this is arrow function , store in func name variable.
+
+
+//2.fat arrow function with one parameters
+var b = (parm)=>{}; // one variable k case me hum ise hata sakte hai, parm k bracket ko 
+
+//3.fat arrow function with implicit return
+var g = ()=>12;
+console.log(g());
