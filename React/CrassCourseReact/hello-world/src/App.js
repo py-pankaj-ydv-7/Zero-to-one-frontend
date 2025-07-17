7ab518241fa59f85ba0ca8143fb1237d8214e236
Pankaj Yadav <pankajyadav851685@gmail.com>
2025-07-17 15:20:29 +0530

    react js , intro  and component

---
+++ React/CrassCourseReact/hello-world/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from './components/Button';
+
+function App() {
+  return ( <>
+    
+    <div className="App">
+      <p>this is pragraph</p>
+      <div>
+        <h1>My App Component</h1>
+      </div>
+      <h1>Hello from React App</h1>
+    </div>
+    <Button/>
+    </>
+  );
+  
+}
+
+export default App;
---
+++ React/CrassCourseReact/hello-world/src/components/Button.jsx
@@ -0,0 +1,6 @@
+const Button =() =>{
+    return <button>Click Me I am a custom Button Component</button>;
+
+};
+
+export default Button
